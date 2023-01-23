6859e5d0e3d8bf0747434b3fccbbcd4821f55fb6
dcode3244 <dagimourgie111@gmail.com>
2023-01-23 15:07:25 +0000

    task 2 final

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,16 +15,30 @@ def do_deploy(archive_path):
         return False
     if put('{}'.format(archive_path), '/tmp/').failed is True:
         return False
-    name = archive_path.replace('.tgz', '')
-    name = name.replace('versions/', '')
-    if run('tar -xzvf /tmp/web_static*.tgz -C \
-            /data/web_static/releases/'.format(name)).failed is True:
+    tgzfile = archive_path.split('/')[-1]
+    name = tgzfile.split('.')[0]
+    if run('mkdir -p /data/web_static/releases/{}/'
+            .format(name)).failed is True:
         return False
-    if run('rm -f /tmp/web_static*').failed is True:
+    if run('rm -rf /data/web_static/releases/{}/*'
+            .format(name)).failed is True:
+        return False
+    if run('tar -xzf /tmp/{} -C \
+            /data/web_static/releases/{}/'
+            .format(tgzfile, name)).failed is True:
+        return False
+    if run('rm -f /tmp/{}'.format(tgzfile)).failed is True:
+        return False
+    if run('mv -f /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/'
+            .format(name, name)).failed is True:
+        return False
+    if run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(name)).failed is True:
         return False
     if run('rm -rf /data/web_static/current').failed is True:
         return False
-    if run('ln -sf /data/web_static/releases/web_static* \
-            /data/web_static/current').failed is True:
+    if run('ln -sf /data/web_static/releases/{}/ \
+            /data/web_static/current'.format(name)).failed is True:
         return False
     return True
