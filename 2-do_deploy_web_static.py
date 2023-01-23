ac8293932dd692f4eeef86a3ee9c6f629804ac55
dcode3244 <dagimourgie111@gmail.com>
2023-01-23 12:46:18 +0000

    bug fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,28 +4,12 @@ an archive to web server
 '''
 import os
 from fabric.api import *
-from datetime import datetime
 
 env.user = 'ubuntu'
 env.hosts = ['54.237.104.37', '35.174.176.205']
 
 
-def do_pack():
-    " generates .tgz archive "
-    date = datetime.utcnow()
-    path = 'versions/web_static{}{}{}{}{}{}.tgz'.format(date.year, date.month,
-                                                        date.day, date.hour,
-                                                        date.minute,
-                                                        date.second)
-    local("mkdir -p versions")
-    a = local("tar -czvf {} web_static".format(path))
-    if a.failed:
-        return None
-    else:
-        return path
-
-
-def do_deploy(archive_path=''):
+def do_deploy(archive_path):
     " distributes an arvhibe to web servers "
     if os.path.exists(archive_path) is False:
         return False
