a192650dd37feaf1726520eb06d878ebd92afa4b
dcode3244 <dagimourgie111@gmail.com>
2023-01-23 11:01:54 +0000

    pycodestyle fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,15 +4,17 @@
 from fabric.api import *
 from datetime import datetime
 
+
 def do_pack():
-	" generates .tgz archive "
-	date = datetime.utcnow()
-	path = 'versions/web_static{}{}{}{}{}{}.tgz'.format(date.year, date.month,
-							    date.day, date.hour,
-							    date.minute, date.second)
-	local("mkdir -p versions")
-	a = local("tar -czvf {} web_static".format(path))
-	if a.failed:
-		return None
-	else:
-		return path
+    " generates .tgz archive "
+    date = datetime.utcnow()
+    path = 'versions/web_static{}{}{}{}{}{}.tgz'.format(date.year, date.month,
+                                                        date.day, date.hour,
+                                                        date.minute,
+                                                        date.second)
+    local("mkdir -p versions")
+    a = local("tar -czvf {} web_static".format(path))
+    if a.failed:
+        return None
+    else:
+        return path
