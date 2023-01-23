c7b8af52bd9567882f2d3941d275bd0dc3b7db1f
dcode3244 <dagimourgie111@gmail.com>
2023-01-23 10:47:10 +0000

    task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# a Fabric script that generates a .tgz archive.
+
+from fabric.api import *
+from datetime import datetime
+
+def do_pack():
+	" generates .tgz archive "
+	date = datetime.utcnow()
+	path = 'versions/web_static{}{}{}{}{}{}.tgz'.format(date.year, date.month,
+							    date.day, date.hour,
+							    date.minute, date.second)
+	local("mkdir -p versions")
+	a = local("tar -czvf {} web_static".format(path))
+	if a.failed:
+		return None
+	else:
+		return path
