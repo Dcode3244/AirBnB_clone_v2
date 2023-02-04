d235bf4f9d85805800dbfa0d92c728612fa9eefb
dcode3244 <dagimourgie111@gmail.com>
2023-02-04 12:21:33 +0000

    task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" starts a Flask web application on 0.0.0.0:5000
+    Routes:
+        /: displays 'Hello HBNB!'
+        /hbnb: displays 'HBNB'
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c(text):
+    return "C %s" % text.replace("_", " ")
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python(text="is cool"):
+    return "Python %s" % text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
