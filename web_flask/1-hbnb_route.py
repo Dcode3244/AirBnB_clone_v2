a8ef98a9dd95248d1b4b7f9b87df4913ebe7cd92
dcode3244 <dagimourgie111@gmail.com>
2023-02-04 08:37:15 +0000

    task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" starts a Flask web application on 0.0.0.0:5000
+    Routes:
+        /: displays 'Hello HBNB!'
+        /hbnb: displays 'HBNB'
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
