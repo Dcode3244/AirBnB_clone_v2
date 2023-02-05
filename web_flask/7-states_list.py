9be9b02ba073831b1b32a26815c50ee4eeb75d59
dcode3244 <dagimourgie111@gmail.com>
2023-02-05 21:13:59 +0000

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,9 +10,23 @@ starts a Flask web application on 0.0.0.0:5000
 						State: <state.id>: <B><state.name></B>
 """
 
-from flask import Flask
+from models import storage
+from flask import Flask, render_template
+
 app = Flask(__name__)
 app.url_map.strict_slashes=False
 
 
-@app.royte
+@app.route('/states_list')
+def states_list():
+	states = storage.all("State")
+	return render_template('7-states_list.html')
+
+
+@app.teardown_appcontext
+def teardown(exeption):
+	storage.close()
+
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0")
