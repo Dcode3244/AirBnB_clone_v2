2ec63660afd3187277356e4830fe900086e30b04
dcode3244 <dagimourgie111@gmail.com>
2023-02-06 10:03:42 +0000

    style fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
 starts a Flask web application on 0.0.0.0:5000
-	Routes:
-		/states_list: displays HTML page: with list of State objects
+    Routes:
+        /states_list: displays HTML page: with list of State objects
 """
 
 from models import storage
@@ -11,19 +11,19 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes=False
+app.url_map.strict_slashes = False
 
 
 @app.route('/states_list')
 def states_list():
-	states = storage.all(State)
-	return render_template('7-states_list.html', states=states)
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def teardown(exc):
-	storage.close()
+    storage.close()
 
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0")
